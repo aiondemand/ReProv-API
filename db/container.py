68e4db63c63d4594c8405196c70bbbb86ae44ffb
aganios <ant.ganios@gmail.com>
2024-01-19 13:01:01 +0200

    user must either provide url:tag OR dockerfile when registering container

--- db/container.py
+++ db/container.py
@@ -1,13 +1,28 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 from .init_db import Base
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, root_validator
+
 
 class ContainerModel(BaseModel):
-    url: str
-    tag: str = 'latest'
+    url: str = None
+    tag: str = None
     name: str = None
     dockerfile: str = None
+
+    @root_validator(pre=True)
+    def check_exclusive_fields(cls, values):
+        url = values.get("url")
+        tag = values.get("tag")
+        dockerfile = values.get("dockerfile")
+
+        if url is not None and tag is not None and dockerfile is not None:
+            raise ValueError("Either 'url' and 'tag' or 'dockerfile' should be provided, not both.")
+
+        if (url is None or tag is None) and dockerfile is None:
+            raise ValueError("Either 'url' and 'tag' or 'dockerfile' should be provided.")
+
+        return values
     class Config:
         orm_mode = True
 
@@ -18,7 +33,7 @@ class Container(Base):
     )
 
     id = Column(Integer, primary_key=True, index=True)
-    url = Column(String, nullable=False)
-    tag = Column(String, default='latest', nullable=True)
+    url = Column(String, nullable=True)
+    tag = Column(String, nullable=True)
     name = Column(String, nullable=True)
     dockerfile = Column(String, nullable=True)
