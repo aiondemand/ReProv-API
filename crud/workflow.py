10f1790b31d3a2902072d84cf52088d8d2ee3c0a
aganios <ant.ganios@gmail.com>
2024-02-07 13:10:28 +0200

    workflow is now registered in reana as well

--- crud/workflow.py
+++ crud/workflow.py
@@ -1,12 +1,19 @@
-
+import os
 from fastapi import APIRouter, UploadFile, File, HTTPException, status, Depends
 from db.workflow import Workflow, WorkflowModel
 from db.user import User
 from db.init_db  import session
 from sqlalchemy.exc import IntegrityError
 from reana_client.api import client
+import tempfile
 
 router = APIRouter()
+
+@router.get("/test")
+async def test():
+    out = client.ping(access_token=os.environ['REANA_ACCESS_TOKEN'])
+    print(out)
+
 
 @router.get("/", response_model=list[WorkflowModel])
 async def get_all_workflows(skip: int = 0, limit: int = 10):
@@ -29,22 +36,43 @@ async def get_workflow_by_id(workflow_id: int):
 
 @router.post("/register/")
 async def register_workflow(workflow: WorkflowModel = Depends(), spec_file: UploadFile = File(...), input_file: UploadFile = File(None)):
-    try:
-        db_workflow = Workflow(name=workflow.name, version=workflow.version, spec_file=spec_file.file.read(), input_file = input_file.file.read() if input_file else None)
-        session.add(db_workflow)
-        session.commit()
-        session.refresh(db_workflow)
-        return {
-                    "New Workflow registered with ID": db_workflow.id
-                }
-    except IntegrityError as e:
-        session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail="Integrity error. Duplicate URL and tag combination."
-        ) from e
-        
+    spec_file_content = spec_file.file.read()
+    input_file_content = input_file.file.read() if input_file else None
+    with tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix='.cwl',delete=False) as spec_temp_file:
+        spec_temp_file.write(spec_file_content)
 
+    try:
+       
+        reana_workflow = client.create_workflow_from_json(
+                name=workflow.name,
+                access_token=os.environ['REANA_ACCESS_TOKEN'],
+                workflow_file=os.path.join(os.getcwd(),spec_temp_file.name),
+                workflow_engine='cwl'
+            )
+        
+        db_workflow = Workflow(name=workflow.name, version=workflow.version, reana_id=reana_workflow['workflow_id'], reana_name=reana_workflow['workflow_name'], spec_file=spec_file_content, input_file = input_file_content)
+        try:
+            session.add(db_workflow)
+            session.commit()
+            session.refresh(db_workflow)
+        except IntegrityError as e:
+            session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                detail="Integrity error. Duplicate name and version combination."
+            ) from e
+
+    finally:
+        os.remove(os.path.join(os.getcwd(),spec_temp_file.name))
+        return {
+            "New Workflow registered with":{
+                "ID": db_workflow.id,
+                "Name / Version": f"{workflow.name} / {workflow.version}",
+                "Reana ID": reana_workflow['workflow_id'],
+                "Reana Name": reana_workflow['workflow_name'],
+            } 
+        }
+ 
 
 @router.post("/execute/")
 async def execute_workflow(user_id: str, workflow_id: int):
--- db/workflow.py
+++ db/workflow.py
@@ -9,16 +9,18 @@ class WorkflowModel(BaseModel):
     class Config:
         orm_mode = True
 
+
 class Workflow(Base):
     __tablename__ = "workflow"
     # __table_args__ = (
     #     UniqueConstraint('name', 'version', name='uq_name_version'),
     # )
 
-
  
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True) # PK of table
+    reana_id = Column(String, nullable=False) # ID assigned to workflow by REANA
     name = Column(String, nullable=False)
+    reana_name = Column(String, nullable=False) # name assigned to workflow by REANA
     version = Column(String, nullable=False)
     spec_file = Column(String, nullable=False)
     input_file = Column(String, nullable=True)
