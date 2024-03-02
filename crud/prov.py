fd9fe04b3cc02631f578ce83512a5e32e72a0033
aganios <ant.ganios@gmail.com>
2024-03-02 18:32:29 +0200

    capture provenance in db

--- crud/prov.py
+++ crud/prov.py
@@ -1,9 +1,12 @@
 import os
+import pandas as pd
 from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException, status
-from db.prov import Entity
+from db.prov import Entity, Activity
 from db.init_db  import session
-from db.workflow_execution import WorkflowExecution
+from db.workflow_execution import WorkflowExecution, WorkflowExecutionStep
+from db.workflow_registry import WorkflowRegistry
+from ruamel.yaml import YAML
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from reana_client.api import client
@@ -18,6 +21,7 @@ router = APIRouter()
 )
 async def track_provenance(reana_name: str, run_number:int):
 	workflow_execution = session.query(WorkflowExecution).filter(WorkflowExecution.reana_name == reana_name, WorkflowExecution.reana_run_number == run_number).first()
+	workflow_execution_steps = session.query(WorkflowExecutionStep).filter(WorkflowExecutionStep.workflow_execution_id == workflow_execution.id).all()
 
 	if workflow_execution is None:
 		raise HTTPException(
@@ -38,10 +42,16 @@ async def track_provenance(reana_name: str, run_number:int):
 		workflow=workflow_execution.reana_id,
 		access_token=os.environ['REANA_ACCESS_TOKEN']
 	)
-	output_files = [f for f in workflow_files if f['name'].startswith('outputs/')]
+	output_files = [f for f in workflow_files if f['name'].startswith('outputs/') and f['name'].split('/')[-1] != 'map.txt' ]
 	intermediate_files = [f for f in workflow_files if f['name'].startswith('cwl/')]
 	spec_file = [f for f in workflow_files if f['name'] == 'workflow.json'][0]
+	map_file_content = client.download_file(
+		workflow=workflow_execution.reana_id,
+		access_token=os.environ['REANA_ACCESS_TOKEN'],
+		file_name='outputs/map.txt'
+	)[0].decode('utf-8').split('\n')
 
+	map_df = pd.DataFrame([line.split(',') for line in map_file_content if line], columns=['filename', 'entity_name'])
 
 	# create entity for the whole workflow
 	workflow_entity = Entity(
@@ -49,9 +59,8 @@ async def track_provenance(reana_name: str, run_number:int):
 		path=f"/var/reana/users/00000000-0000-0000-0000-000000000000/workflows/{workflow_execution.reana_id}/workflow.json",
 		name='workflow.json',
 		size=spec_file['size']['human_readable'],
-		last_modified=spec_file['last-modified']
+		last_modified=datetime.fromisoformat(spec_file['last-modified'])
 	)
-	session.add(workflow_entity)
 
 	# create entities for the intermediate files
 	intermediate_entities = [
@@ -60,12 +69,10 @@ async def track_provenance(reana_name: str, run_number:int):
 			path=i_file['name'],
 			name=i_file['name'].split('/')[-1],
 			size=i_file['size']['human_readable'],
-			last_modified=i_file['last-modified']
+			last_modified=datetime.fromisoformat(i_file['last-modified'])
 		) for i_file in intermediate_files
 	]
-	for i_entity in intermediate_entities:
-		session.add(i_entity)
-
+	
 
 	# create entities for the final output files
 	output_entities = [
@@ -74,11 +81,58 @@ async def track_provenance(reana_name: str, run_number:int):
 			path=o_file['name'],
 			name=o_file['name'].split('/')[-1],
 			size=o_file['size']['human_readable'],
-			last_modified=o_file['last-modified']
+			last_modified=datetime.fromisoformat(o_file['last-modified'])
 		) for o_file in output_files
 	]
-	for o_entity in output_entities:
-		session.add(o_entity)
+	
+	entities = [workflow_entity] + intermediate_entities + output_entities
 
+	activities = [
+		Activity(
+			type='step_execution',
+			name=s.name,
+			start_time=s.start_time,
+			end_time=s.end_time
+		) for s in workflow_execution_steps if s.name != 'map'
+	]
+
+	for e in entities:
+		session.add(e)
+	for a in activities:
+		session.add(a)
+
+	session.commit()
+
+
+	workflow_spec_file = session.query(WorkflowRegistry).filter(WorkflowRegistry.id == workflow_execution.registry_id).first().spec_file_content
+	yaml = YAML(typ='safe', pure=True)
+	data = yaml.load(workflow_spec_file)
+	for s in data['steps'] :
+		if s == 'map': # ignore map step
+			continue
+
+		step_file_inputs = [key for key, value in data['steps'][s]['run']['inputs'].items() if value == 'File']
+		step_file_outputs =[o['id'] for o in data['steps'][s]['run']['outputs'] if o['type'] == 'File']
+
+		# for each input file in step (f):
+		# this file wasUsedBy the corresponding entity (eneity_name) with filename=map_df.loc['filename'=f]
+
+		for f in step_file_inputs:
+			entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
+			entity = [e for e in entities if e.name==entity_name][0]
+			activity = [a for a in activities if a.name==s][0]
+			activity.used.append(entity)
+			session.add(activity)
+
+		
+		for f in step_file_outputs:
+			entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
+			entity = [e for e in entities if e.name==entity_name][0]
+			activity = [a for a in activities if a.name==s][0]
+
+			activity.generated.append(entity)
+			session.add(activity)
+	
+	session.commit()
 
 	return {}
--- db/prov.py
+++ db/prov.py
@@ -7,10 +7,9 @@ from sqlalchemy.orm import relationship
 
 
 
-
 class Entity(Base):
     __tablename__ = 'entity'
-    
+
     id = Column(Integer, autoincrement=True, primary_key=True)
     type = Column(Enum('workflow', 'workflow_intermediate_result_file', 'workflow_final_result_file'))
     path = Column(String, nullable=False)
@@ -18,26 +17,30 @@ class Entity(Base):
     size = Column(Integer, nullable=True)
     last_modified = Column(DateTime, nullable=True)
 
-    used_by = relationship('Activity', secondary='entity_activity', back_populates='generated_by')
-
-
-
-
 class Activity(Base):
     __tablename__ = 'activity'
-    
+
     id = Column(Integer, autoincrement=True, primary_key=True)
     type = Column(Enum('workflow_execution', 'step_execution'))
-    start_time = Column(DateTime, default=datetime.utcnow,)
+    name = Column(String, nullable=False)
+    start_time = Column(DateTime, default=datetime.utcnow, nullable=False)  
     end_time = Column(DateTime, nullable=True)
 
-    generated_by = relationship('Entity', secondary='entity_activity', back_populates='used_by')
+    # Use backref directly
+    generated = relationship("Entity",secondary='entity_generated_by')
+    used = relationship("Entity", secondary='entity_used_by')
 
-
-
-class EntityActivity(Base):
-    __tablename__ = "entity_activity"
+class EntityUsedBy(Base):
+    __tablename__ = "entity_used_by"
 
     id = Column(Integer, primary_key=True)
-    entity_id = Column(String, ForeignKey('entity.id'))
-    activity_id = Column(Integer, ForeignKey('activity.id'))
+    entity_id = Column(Integer, ForeignKey('entity.id'))  
+    activity_id = Column(Integer, ForeignKey('activity.id'))  
+
+
+class EntityGeneratedBy(Base):
+    __tablename__ = "entity_generated_by"
+
+    id = Column(Integer, primary_key=True)
+    entity_id = Column(Integer, ForeignKey('entity.id')) 
+    activity_id = Column(Integer, ForeignKey('activity.id'))  
