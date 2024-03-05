9aff1b54f15a0cc48b7fa1aecb4b005f5380ee80
aganios <ant.ganios@gmail.com>
2024-03-05 12:39:18 +0200

    style changes in prov.py file

--- crud/prov.py
+++ crud/prov.py
@@ -17,230 +17,228 @@ router = APIRouter()
 
 
 @router.get(
-	"/capture/",
-	description="Capture provenance for workflow with specific name & run number",
+    "/capture/",
+    description="Capture provenance for workflow with specific name & run number",
 )
 async def track_provenance(reana_name: str, run_number: int):
-	workflow_execution = session.query(WorkflowExecution).filter(
-		WorkflowExecution.reana_name == reana_name, 
-		WorkflowExecution.reana_run_number == run_number
-	).first()
-	if workflow_execution is None:
-		raise HTTPException(
+    workflow_execution = session.query(WorkflowExecution).filter(
+        WorkflowExecution.reana_name == reana_name,
+        WorkflowExecution.reana_run_number == run_number
+    ).first()
+    if workflow_execution is None:
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Workflow with name {reana_name} and run number {run_number} was not found",
         )
 
-	execution_status = workflow_execution.status
+    execution_status = workflow_execution.status
 
-	if execution_status != 'finished':
-		raise HTTPException(
+    if execution_status != 'finished':
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Workflow with name {reana_name} and run number {run_number} must be finished in order to capture provenance",
         )
-	
-	previously_captured = session.query(Activity).filter(Activity.workflow_execution_id == workflow_execution.id).first()
-	if previously_captured:
-		raise HTTPException(
+
+    previously_captured = session.query(Activity).filter(Activity.workflow_execution_id == workflow_execution.id).first()
+    if previously_captured:
+        raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail=f"Provenance for workflow with name {reana_name} and run number {run_number} was captured before",
         )
 
-	workflow_execution_steps = session.query(WorkflowExecutionStep).filter(WorkflowExecutionStep.workflow_execution_id == workflow_execution.id).all()
+    workflow_execution_steps = session.query(WorkflowExecutionStep).filter(WorkflowExecutionStep.workflow_execution_id == workflow_execution.id).all()
 
-	
-	workflow_files = client.list_files(
-		workflow=workflow_execution.reana_id,
-		access_token=os.environ['REANA_ACCESS_TOKEN']
-	)
-	output_files = [f for f in workflow_files if f['name'].startswith('outputs/') and f['name'].split('/')[-1] != 'map.txt' ]
-	intermediate_files = [f for f in workflow_files if f['name'].startswith('cwl/')]
-	spec_file = [f for f in workflow_files if f['name'] == 'workflow.json'][0]
-	map_file_content = client.download_file(
-		workflow=workflow_execution.reana_id,
-		access_token=os.environ['REANA_ACCESS_TOKEN'],
-		file_name='outputs/map.txt'
-	)[0].decode('utf-8').split('\n')
+    workflow_files = client.list_files(
+        workflow=workflow_execution.reana_id,
+        access_token=os.environ['REANA_ACCESS_TOKEN']
+    )
+    output_files = [f for f in workflow_files if f['name'].startswith('outputs/') and f['name'].split('/')[-1] != 'map.txt']
+    intermediate_files = [f for f in workflow_files if f['name'].startswith('cwl/')]
+    spec_file = [f for f in workflow_files if f['name'] == 'workflow.json'][0]
+    map_file_content = client.download_file(
+        workflow=workflow_execution.reana_id,
+        access_token=os.environ['REANA_ACCESS_TOKEN'],
+        file_name='outputs/map.txt'
+    )[0].decode('utf-8').split('\n')
 
-	map_df = pd.DataFrame([line.split(',') for line in map_file_content if line], columns=['filename', 'entity_name'])
+    map_df = pd.DataFrame([line.split(',') for line in map_file_content if line], columns=['filename', 'entity_name'])
 
-	# create entity for the whole workflow
-	workflow_entity = Entity(
-		type='workflow',
-		path=f"/var/reana/users/00000000-0000-0000-0000-000000000000/workflows/{workflow_execution.reana_id}/workflow.json",
-		name='workflow.json',
-		size=spec_file['size']['human_readable'],
-		last_modified=datetime.fromisoformat(spec_file['last-modified'])
-	)
+    # create entity for the whole workflow
+    workflow_entity = Entity(
+        type='workflow',
+        path=f"/var/reana/users/00000000-0000-0000-0000-000000000000/workflows/{workflow_execution.reana_id}/workflow.json",
+        name='workflow.json',
+        size=spec_file['size']['human_readable'],
+        last_modified=datetime.fromisoformat(spec_file['last-modified'])
+    )
 
-	# create entities for the intermediate files
-	intermediate_entities = [
-		Entity(
-			type='workflow_intermediate_result_file',
-			path=i_file['name'],
-			name=i_file['name'].split('/')[-1],
-			size=i_file['size']['human_readable'],
-			last_modified=datetime.fromisoformat(i_file['last-modified']),
-			workflow_execution_id=workflow_execution.id
-		) for i_file in intermediate_files
-	]
-	
+    # create entities for the intermediate files
+    intermediate_entities = [
+        Entity(
+            type='workflow_intermediate_result_file',
+            path=i_file['name'],
+            name=i_file['name'].split('/')[-1],
+            size=i_file['size']['human_readable'],
+            last_modified=datetime.fromisoformat(i_file['last-modified']),
+            workflow_execution_id=workflow_execution.id
+        ) for i_file in intermediate_files
+    ]
 
-	# create entities for the final output files
-	output_entities = [
-		Entity(
-			type='workflow_final_result_file',
-			path=o_file['name'],
-			name=o_file['name'].split('/')[-1],
-			size=o_file['size']['human_readable'],
-			last_modified=datetime.fromisoformat(o_file['last-modified']),
-			workflow_execution_id=workflow_execution.id
-		) for o_file in output_files
-	]
-	
-	entities = [workflow_entity] + intermediate_entities + output_entities
+    # create entities for the final output files
+    output_entities = [
+        Entity(
+            type='workflow_final_result_file',
+            path=o_file['name'],
+            name=o_file['name'].split('/')[-1],
+            size=o_file['size']['human_readable'],
+            last_modified=datetime.fromisoformat(o_file['last-modified']),
+            workflow_execution_id=workflow_execution.id
+        ) for o_file in output_files
+    ]
 
-	step_activities = [
-		Activity(
-			type='step_execution',
-			name=s.name,
-			start_time=s.start_time,
-			end_time=s.end_time,
-			workflow_execution_id=workflow_execution.id
-		) for s in workflow_execution_steps if s.name != 'map'
-	]
-	workflow_activity = Activity(
-		type='workflow_execution',
-		name=f"{workflow_execution.reana_name}.{workflow_execution.reana_run_number}",
-		start_time=workflow_execution.start_time,
-		end_time=workflow_execution.end_time,
-		workflow_execution_id=workflow_execution.id
-	)
-	activities = [workflow_activity] + step_activities
+    entities = [workflow_entity] + intermediate_entities + output_entities
 
-	for e in entities:
-		session.add(e)
-	for a in activities:
-		session.add(a)
+    step_activities = [
+        Activity(
+            type='step_execution',
+            name=s.name,
+            start_time=s.start_time,
+            end_time=s.end_time,
+            workflow_execution_id=workflow_execution.id
+        ) for s in workflow_execution_steps if s.name != 'map'
+    ]
+    workflow_activity = Activity(
+        type='workflow_execution',
+        name=f"{workflow_execution.reana_name}.{workflow_execution.reana_run_number}",
+        start_time=workflow_execution.start_time,
+        end_time=workflow_execution.end_time,
+        workflow_execution_id=workflow_execution.id
+    )
+    activities = [workflow_activity] + step_activities
 
-	workflow_spec_file = session.query(WorkflowRegistry).filter(WorkflowRegistry.id == workflow_execution.registry_id).first().spec_file_content
-	yaml = YAML(typ='safe', pure=True)
-	data = yaml.load(workflow_spec_file)
-	for s in data['steps'] :
-		if s == 'map': # ignore map step
-			continue
+    for e in entities:
+        session.add(e)
+    for a in activities:
+        session.add(a)
 
-		step_file_inputs = [key for key, value in data['steps'][s]['run']['inputs'].items() if value == 'File']
-		step_file_outputs =[o['id'] for o in data['steps'][s]['run']['outputs'] if o['type'] == 'File']
+    workflow_spec_file = session.query(WorkflowRegistry).filter(WorkflowRegistry.id == workflow_execution.registry_id).first().spec_file_content
+    yaml = YAML(typ='safe', pure=True)
+    data = yaml.load(workflow_spec_file)
+    for s in data['steps']:
+        if s == 'map':  # ignore map step
+            continue
 
-		# for each input file in step (f):
-		# this file wasUsedBy the corresponding entity (eneity_name) with filename=map_df.loc['filename'=f]
+        step_file_inputs = [key for key, value in data['steps'][s]['run']['inputs'].items() if value == 'File']
+        step_file_outputs = [o['id'] for o in data['steps'][s]['run']['outputs'] if o['type'] == 'File']
 
-		for f in step_file_inputs:
-			entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
-			entity = [e for e in entities if e.name==entity_name][0]
-			activity = [a for a in step_activities if a.name==s][0]
-			activity.used.append(entity)
-			session.add(activity)
+        # for each input file in step (f):
+        # this file wasUsedBy the corresponding entity (eneity_name) with filename=map_df.loc['filename'=f]
 
-		
-		for f in step_file_outputs:
-			entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
-			entity = [e for e in entities if e.name==entity_name][0]
-			activity = [a for a in step_activities if a.name==s][0]
+        for f in step_file_inputs:
+            entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
+            entity = [e for e in entities if e.name == entity_name][0]
+            activity = [a for a in step_activities if a.name == s][0]
+            activity.used.append(entity)
+            session.add(activity)
 
-			activity.generated.append(entity)
-			session.add(activity)
-	
-	for e in entities:
-		# workflow used every entity
-		workflow_activity.used.append(e)
-		# workflow generated every entity that was not input on first step
-		workflow_activity.generated.append(e)
-		session.add(workflow_activity)
+        for f in step_file_outputs:
+            entity_name = map_df.loc[map_df['filename'] == f].to_dict('records')[0]['entity_name']
+            entity = [e for e in entities if e.name == entity_name][0]
+            activity = [a for a in step_activities if a.name == s][0]
 
-	workflow_entity.started.append(workflow_activity)
-	workflow_entity.ended.append(workflow_activity)
+            activity.generated.append(entity)
+            session.add(activity)
 
-	session.commit()
+    for e in entities:
+        # workflow used every entity
+        workflow_activity.used.append(e)
+        # workflow generated every entity that was not input on first step
+        workflow_activity.generated.append(e)
+        session.add(workflow_activity)
 
-	return {
-		"Provenance was succesfully captured for workflow with":{
-			"Reana Name": reana_name,
-			"Run Number": run_number
-		} 
-	}
+    workflow_entity.started.append(workflow_activity)
+    workflow_entity.ended.append(workflow_activity)
 
+    session.commit()
+
+    return {
+        "Provenance was succesfully captured for workflow with": {
+            "Reana Name": reana_name,
+            "Run Number": run_number
+        }
+    }
 
 
 @router.get(
-	"/draw/",
-	description="Create a graphical represenation of provenance for workflow with specific reana_name and run number",
+    "/draw/",
+    description="Create a graphical represenation of provenance for workflow with specific reana_name and run number",
 )
-async def draw_provenance(reana_name: str, run_number:int):
-	workflow_execution = session.query(WorkflowExecution).filter(WorkflowExecution.reana_name == reana_name, WorkflowExecution.reana_run_number == run_number).first()
-	if workflow_execution is None:
-		raise HTTPException(
+async def draw_provenance(reana_name: str, run_number: int):
+    workflow_execution = session.query(WorkflowExecution).filter(
+        WorkflowExecution.reana_name == reana_name,
+        WorkflowExecution.reana_run_number == run_number
+    ).first()
+    if workflow_execution is None:
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Workflow with name {reana_name} and run number {run_number} was not found",
         )
-	
 
-	activities = session.query(Activity).filter(Activity.workflow_execution_id==workflow_execution.id).all()
-	entities = session.query(Entity).filter(Entity.workflow_execution_id==workflow_execution.id).all()
+    activities = session.query(Activity).filter(
+        Activity.workflow_execution_id == workflow_execution.id
+    ).all()
+    entities = session.query(Entity).filter(
+        Entity.workflow_execution_id == workflow_execution.id
+    ).all()
 
-	
+    doc = ProvDocument()
+    doc.set_default_namespace('https://www.w3.org/TR/prov-dm/')
 
-	doc = ProvDocument() 
-	doc.set_default_namespace('https://www.w3.org/TR/prov-dm/')
+    for e in entities:
+        doc.entity(
+            e.name,
+            {
+                'id': e.id,
+                'type': e.type,
+                'path': e.path,
+                'name': e.name,
+                'size': e.size,
+                'last_modified': e.last_modified,
+            }
+        )
 
-	for e in entities:
-		doc.entity(
-			e.name,
-			{
-				'id': e.id,
-				'type': e.type,
-				'path': e.path,
-				'name': e.name,
-				'size': e.size,
-				'last_modified': e.last_modified,
-			}
-		)
-		
-	for a in activities:
-		doc.activity(
-			a.name.replace(':','_'), 
-			a.start_time,
-			a.end_time,
-			{
-				'id': a.id,
-				'type': a.type
-			}
-		)
-		
-	for e in entities:
-		generated_by = session.query(EntityGeneratedBy).filter(EntityGeneratedBy.entity_id==e.id).all()
-		for g in generated_by:
-			entity_name = session.query(Entity.name).filter(Entity.id==g.entity_id).first()[0].replace(':','_')
-			activity_name = session.query(Activity.name).filter(Activity.id==g.activity_id).first()[0].replace(':','_')
-			doc.wasGeneratedBy(entity_name,activity_name)
+    for a in activities:
+        doc.activity(
+            a.name.replace(':', '_'),
+            a.start_time,
+            a.end_time,
+            {
+                'id': a.id,
+                'type': a.type
+            }
+        )
 
-		used_by = session.query(EntityUsedBy).filter(EntityUsedBy.entity_id==e.id).all()
-		for u in used_by:
-			entity_name = session.query(Entity.name).filter(Entity.id==u.entity_id).first()[0].replace(':','_')
-			activity_name = session.query(Activity.name).filter(Activity.id==u.activity_id).first()[0].replace(':','_')
-			doc.used(entity_name,activity_name)
+    for e in entities:
+        generated_by = session.query(EntityGeneratedBy).filter(EntityGeneratedBy.entity_id == e.id).all()
+        for g in generated_by:
+            entity_name = session.query(Entity.name).filter(Entity.id == g.entity_id).first()[0].replace(':', '_')
+            activity_name = session.query(Activity.name).filter(Activity.id == g.activity_id).first()[0].replace(':', '_')
+            doc.wasGeneratedBy(entity_name, activity_name)
 
+        used_by = session.query(EntityUsedBy).filter(EntityUsedBy.entity_id == e.id).all()
+        for u in used_by:
+            entity_name = session.query(Entity.name).filter(Entity.id == u.entity_id).first()[0].replace(':', '_')
+            activity_name = session.query(Activity.name).filter(Activity.id == u.activity_id).first()[0].replace(':', '_')
+            doc.used(entity_name, activity_name)
 
-	png_name = f"{reana_name}:{run_number}-provenance.png"
-	dot = prov_to_dot(doc).write_png(png_name)
-	def _delete_png_file():
-		os.unlink(png_name)
+    png_name = f"{reana_name}:{run_number}-provenance.png"
+    prov_to_dot(doc).write_png(png_name)
 
+    def _delete_png_file():
+        os.unlink(png_name)
 
-	return FileResponse(
-		png_name, 
-		filename=png_name,
-		background=BackgroundTask(_delete_png_file),
+    return FileResponse(
+        png_name,
+        filename=png_name,
+        background=BackgroundTask(_delete_png_file),
     )
-
