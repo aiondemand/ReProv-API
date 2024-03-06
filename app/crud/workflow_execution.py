ced7ff5d722c4c9bd327bad29a573e40b514c10b
aganios <ant.ganios@gmail.com>
2024-03-06 15:04:00 +0200

    application is availabe through containerized environment

--- app/crud/workflow_execution.py
+++ app/crud/workflow_execution.py
@@ -118,9 +118,7 @@ async def execute_workflow(registry_id: int, background_tasks: BackgroundTasks):
     print(os.environ['REANA_ACCESS_TOKEN'])
     with tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix='.cwl', delete=False) as spec_temp_file:
         spec_temp_file.write(workflow_registry.spec_file_content.encode('utf-8'))
-        print("edwwwwwww")
 
-    print("EDw1")
     inputs = {"parameters": {}}
     if workflow_registry.input_file_content:
         with tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix='.yaml', delete=False) as input_temp_file:
@@ -129,10 +127,7 @@ async def execute_workflow(registry_id: int, background_tasks: BackgroundTasks):
             for line in f:
                 k, v = line.strip().split(": ")
                 inputs["parameters"][k] = v
-    print("EDw2")
     try:
-        print(spec_temp_file.name)
-        print(os.path.join(os.getcwd(), spec_temp_file.name))
         reana_workflow = client.create_workflow_from_json(
             name=f"{workflow_registry.name}:{workflow_registry.version}",
             access_token=os.environ['REANA_ACCESS_TOKEN'],
@@ -140,7 +135,7 @@ async def execute_workflow(registry_id: int, background_tasks: BackgroundTasks):
             parameters=inputs if inputs["parameters"] != {} else None,
             workflow_engine='cwl'
         )
-        
+
     except Exception as e:
         os.remove(os.path.join(os.getcwd(), spec_temp_file.name))
         if workflow_registry.input_file_content:
@@ -245,6 +240,7 @@ async def monitor_execution(reana_id):
         WorkflowExecutionStep.name == current_step,
     ).first()
     last_workflow_execution_step.end_time = datetime.utcnow()
+    last_workflow_execution_step.status = 'finished'
     session.add(last_workflow_execution_step)
     session.commit()
 
--- app/schema/init_db.py
+++ app/schema/init_db.py
@@ -1,11 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@db:3306/db_name"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(os.environ['DATABASE_URL'])
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
