99c59e40f97821a3b74287362c67f4b5b242a5a1
aganios <ant.ganios@gmail.com>
2024-03-26 13:08:18 +0200

    create table for image registry

---
+++ src/schema/image_registry.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.mysql import LONGTEXT
+from .init_db import Base
+from pydantic import BaseModel
+
+
+class ImageRegistry(Base):
+    __tablename__ = "image_registry"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    version = Column(String(255), nullable=False)
+    description = Column(LONGTEXT, nullable=False)
+    username = Column(String(255), nullable=False)
+    group = Column(String(255), nullable=False)
